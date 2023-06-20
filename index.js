8179cd3197d5ab67a8a0606a0f602f551be391d3
Parfo <p.foitzick@gmail.com>
2023-06-19 22:06:43 -0400

    de a poco

---
+++ index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { create } from 'handlebars';
+// import * as helpers from "helpers";
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.resolve(fileURLToPath(import.meta.url));
